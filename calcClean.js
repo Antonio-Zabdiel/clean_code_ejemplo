d3f7ce6c84b5a90ea614f4401f78117a5ca19d05
martin-sainos-demian <martin.sainos.demian@gmail.com>
2023-03-09 14:27:24 -0600

    gfhhjklñ

--- calcClean.js
+++ calcClean.js
@@ -1,25 +1,35 @@
 const calculate = (inicio, final) => {
     //calcular para imprimir a doble cara en js
 
-    inicio = parseInt(inicio)
-    final = parseInt(final)
+    inicio = parseInt(inicio)   //numero de hoja en la que inicia
+    final = parseInt(final)     //numero de hoja en la que termina
 
     var resultado = ""
 
-    for(var p = inicio; p <= final; p+=2){
-        resultado += p + ","
-    }
+    //numeros de página a imprimir al frente
+    resultado = pages(inicio, final)
 
-    console.log("1, 3, 5, ...")
+    console.log("enfrente: ")
     console.log(resultado)
 
     inicio++
     final++
     resultado = ""
 
+    //numeros de página a imprimir al reverso
+    resultado = pages(inicio, final)
+
+    console.log("reverso: ")
+    console.log(resultado)
+}
+
+const pages = (inicio, final) => {
+    var resultado = ""
+
+    //numeros de página
     for(var p = inicio; p <= final; p+=2){
         resultado += p + ","
     }
-    console.log("2, 4, 6, ...")
-    console.log(resultado)
+
+    return resultado
 }
