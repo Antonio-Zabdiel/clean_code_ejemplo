a19946e96b888a1f6d9b107eb1fad9b1a4e99d4c
Antonio-Zabdiel <112569142+Antonio-Zabdiel@users.noreply.github.com>
2023-03-13 16:32:52 -0600

    Update calc.js

--- js/calc.js
+++ js/calc.js
@@ -5,15 +5,13 @@ var s = ""
 for(var p = i; p <= f; p+=2){
     s += p + ","
 }console.log("1, 3, 5, ...")
-console.log(s)
-i++
-f++
-var r = "enfrente: \n"+s
-s = ""
+console.log(s);
+i++;f++;
+var r = "enfrente: \n"+s;s = "";
 for(var p = i; p <= f; p+=2){
     s += p + ","
 }console.log("2, 4, 6, ...")
 r+="\nreverso: \n"+s
-console.log(s)
+console.log(s);
 return r
 }
